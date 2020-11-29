678b16943143503093d9056eea4ab9fa64289d05
Nicolas Justin <nicolasjustin.dev@gmail.com>
2020-11-29 16:34:10 -0300

    initial commit

---
+++ src/components/SectionConcepts/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import Container from 'components/Container';
+import Heading from 'components/Heading';
+
+import content from './content';
+import * as S from './styles';
+
+const SectionConcepts = () => (
+  <Container>
+    <S.Box>
+      <Heading lineBottom reverseColor>
+        Conceitos que você irá aprender
+      </Heading>
+      <S.List>
+        {content.map((item) => (
+          <S.Item key={item}>{item}</S.Item>
+        ))}
+      </S.List>
+    </S.Box>
+  </Container>
+);
+
+export default SectionConcepts;
---
+++ src/styles/theme.ts
@@ -0,0 +1,44 @@
+export default {
+  grid: {
+    container: '130rem',
+    gutter: '3.2rem',
+  },
+  border: {
+    radius: '0.4rem',
+  },
+  font: {
+    family:
+      "Poppins, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    bold: 600,
+    sizes: {
+      xxsmall: '1.4rem',
+      xsmall: '1.6rem',
+      small: '1.8rem',
+      medium: '2.2rem',
+      large: '2.6rem',
+      xlarge: '3.4rem',
+      xxlarge: '5.2rem',
+    },
+  },
+  colors: {
+    primary: '#f5cb3b',
+    primaryHover: '#c5a22b',
+    secondary: '#f5cb3b',
+    mainBg: '#000',
+    secondaryBg: '#0f0f13',
+    boxBg: '#0c0c0e',
+    texts: '#030517',
+    white: '#FFFFFF',
+    black: '#000000',
+    gray: '#7B7373',
+  },
+  spacings: {
+    xxsmall: '0.8rem',
+    xsmall: '1.6rem',
+    small: '2.4rem',
+    medium: '3.2rem',
+    large: '6.0rem',
+    xlarge: '6.4rem',
+    xxlarge: '12.8rem',
+  },
+};
